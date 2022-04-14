b8bd1bdc1e3458927ec4fdf0b067da407839c22a
kjjee99 <kjjee99@naver.com>
2022-04-14 20:34:12 +0900

    [Feat] Change to Entity Class

--- Board/src/main/java/com/board/Entity/Member.java
+++ Board/src/main/java/com/board/Entity/Member.java
@@ -1,27 +1,50 @@
-package com.board.VO;
+package com.board.Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
+@Table(name = "member")
+@IdClass(memberID.class)
 @ToString
-public class Member {
-	private String id;
-	private String pw;
-	private String name;
-	private String phoneNumber;
-	private String email;
-	private String address;
+public class Member{
+	@Id
+	@Column(name = "idx")
+	private String idx;
+	@Id
+	@Column(name = "userID")
+	private String userID;
+	@Column(name = "userPW")
+	private String userPW;
+	@Column(name = "userName")
+	private String userName;
+	@Column(name = "userPhone")
+	private String userPhone;
+	@Column(name = "userEmail")
+	private String userEmail;
+	@Column(name = "userAddress")
+	private String userAddress;
 	
 	@Builder
 	public Member(String id, String pw, String name, String phoneNumber, String email, String address) {
-		this.id = id;
-		this.pw = pw;
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-		this.address = address;
+		this.userID = id;
+		this.userPW = pw;
+		this.userName = name;
+		this.userPhone = phoneNumber;
+		this.userEmail = email;
+		this.userAddress = address;
 	}
 	
 }
