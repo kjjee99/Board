91245520bc75036774102a2f0b63cfa43e02a7cf
kjjee99 <kjjee99@naver.com>
2022-04-17 22:06:03 +0900

    [Chore]

--- Board/src/main/java/com/board/BoardApplication.java
+++ Board/src/main/java/com/board/BoardApplication.java
@@ -2,9 +2,6 @@ package com.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.board.Ctrl.SignUpController;
 
 @SpringBootApplication
 public class BoardApplication {
