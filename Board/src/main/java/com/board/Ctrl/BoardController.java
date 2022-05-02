10f22a31905bc7630f045f8831e9c49df99d3898
kjjee99 <kjjee99@naver.com>
2022-05-03 00:25:05 +0900

    [Feat] Create Board Controller class
    게시판 controller 클래스 생성
    게시판 보여주는 메서드 생성

---
+++ Board/src/main/java/com/board/Ctrl/BoardController.java
@@ -0,0 +1,31 @@
+package com.board.Ctrl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.board.Entity.Boards;
+import com.board.Service.BoardService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+	@Autowired
+	BoardService service;
+
+	@GetMapping("/list")
+	public ModelAndView showList() {
+		ModelAndView mav = new ModelAndView();
+		List<Boards> list = service.showList();
+		mav.addObject("post", list);
+		mav.setViewName("boardList");
+		return mav;
+	}
+}
