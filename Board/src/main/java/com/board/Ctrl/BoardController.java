628c53eddf591307e141043f1b24b88ddcf95afa
kjjee99 <kjjee99@naver.com>
2022-05-09 00:24:50 +0900

    [Feat] Create post method
    글쓰기 메서드 생성
    저장하는 서비스까지 연결
    redirect로 저장 성공 시 목록으로 돌아가고
    실패 시 글쓰는 화면으로 돌아감

--- Board/src/main/java/com/board/Ctrl/BoardController.java
+++ Board/src/main/java/com/board/Ctrl/BoardController.java
@@ -1,13 +1,18 @@
 package com.board.Ctrl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.board.DTO.BoardDTO;
 import com.board.Entity.Boards;
 import com.board.Service.BoardService;
 
@@ -23,9 +28,40 @@ public class BoardController {
 	@GetMapping("/list")
 	public ModelAndView showList() {
 		ModelAndView mav = new ModelAndView();
-		List<Boards> list = service.showList();
+		List<Boards> list = service.getList();
 		mav.addObject("posts", list);
 		mav.setViewName("/post/boardList");
+		return mav;
+	}
+	
+	@GetMapping("/post")
+	public ModelAndView writePost() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("post", new BoardDTO());
+		mav.setViewName("/post/post_write");
+		return mav;
+	}
+	
+	@PostMapping("/post.do")
+	public ModelAndView doPost(@ModelAttribute BoardDTO post, RedirectAttributes re) {
+		ModelAndView mav = new ModelAndView();
+		Date date = new Date();
+		post.setWriter("aaa");
+		post.setReg_date(date);
+		boolean result = service.savePost(post);
+		if(result) {
+			mav.setViewName("redirect:/board/list");
+		}
+		else {
+			mav.setViewName("redirect:/board/post");
+		}
+		return mav;
+	}
+	
+	@PostMapping()
+	public ModelAndView showPost() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/post/post_detail");
 		return mav;
 	}
 }
