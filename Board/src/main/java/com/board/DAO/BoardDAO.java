d67de9ab029035fde3a655557d82440aac3f7552
kjjee99 <kjjee99@naver.com>
2022-05-21 01:49:31 +0900

    [Feat] Create findByBoardId(int)
    인자가 int인 findById를 생성

--- Board/src/main/java/com/board/DAO/BoardDAO.java
+++ Board/src/main/java/com/board/DAO/BoardDAO.java
@@ -1,5 +1,7 @@
 package com.board.DAO;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.board.Entity.Boards;
 
 @Repository
 public interface BoardDAO extends JpaRepository<Boards, String>{
+
+	Optional<Boards> findByBoardId(int id);
+
+
 
 }
