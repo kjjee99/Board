f5ef44d2803b5ef6318292a76159e46d18f14b72
kjjee99 <kjjee99@naver.com>
2022-05-21 01:50:16 +0900

    [Refactor] Apply Camel Case
    Snake 표기법에서 Camel 표기법으로 변경

--- Board/src/main/java/com/board/DTO/BoardDTO.java
+++ Board/src/main/java/com/board/DTO/BoardDTO.java
@@ -1,6 +1,9 @@
 package com.board.DTO;
 
 import java.util.Date;
+import java.util.Optional;
+
+import com.board.Entity.Boards;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -13,21 +16,22 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 public class BoardDTO {
-	private int board_id;
+	private int boardId;
 	private String writer;
 	private String title;
 	private String content;
-	private Date reg_date;
-	private Date update_date;
-	private Date delete_date;
+	private Date regDate;
+	private Date updateDate;
+	private Date deleteDate;
 	
 	@Builder
 	public BoardDTO(String writer, String title, String content, Date regDate, Date updateDate, Date delDate) {
 		this.writer = writer;
 		this.title = title;
 		this.content = content;
-		this.reg_date = regDate;
-		this.update_date = updateDate;
-		this.delete_date = delDate;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.deleteDate = delDate;
 	}
+	
 }
