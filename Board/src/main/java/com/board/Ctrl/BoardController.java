989b5647891ec22028a78397b082b1e2d91556df
kjjee99 <kjjee99@naver.com>
2022-05-21 18:45:00 +0900

    [Feat] Add comment & Create deletePost and editPost function
    함수 설명에 대한 주석 추가
    게시글 수정하는 함수와 삭제하는 함수 추가

--- Board/src/main/java/com/board/Ctrl/BoardController.java
+++ Board/src/main/java/com/board/Ctrl/BoardController.java
@@ -4,9 +4,11 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,7 @@ public class BoardController {
 	@Autowired
 	BoardService service;
 
+	/* 게시판 목록 보여주기 */
 	@GetMapping("/list")
 	public ModelAndView showList() {
 		ModelAndView mav = new ModelAndView();
@@ -35,6 +38,7 @@ public class BoardController {
 		return mav;
 	}
 	
+	/* 게시글 작성하기 */
 	@GetMapping("/post")
 	public ModelAndView writePost() {
 		ModelAndView mav = new ModelAndView();
@@ -43,6 +47,7 @@ public class BoardController {
 		return mav;
 	}
 	
+	/* 작성한 게시글 저장하기 */
 	@PostMapping("/post.do")
 	public ModelAndView doPost(@ModelAttribute BoardDTO post, RedirectAttributes re) {
 		ModelAndView mav = new ModelAndView();
@@ -59,12 +64,40 @@ public class BoardController {
 		return mav;
 	}
 	
+	/* 게시글 상세보기 */
 	@GetMapping("/detail")
 	public ModelAndView detailPost(@RequestParam("id") int postNumber) {
 		ModelAndView mav = new ModelAndView();
 		BoardDTO post = service.getPost(postNumber);
 		mav.addObject("post", post);
+		System.out.println(post.getRegDate());
 		mav.setViewName("/post/post_detail");
+		return mav;
+	}
+	
+	/* 게시글 수정하기 */
+	@GetMapping("/post/edit")
+	public ModelAndView editPost(@RequestParam("id") int postNumber) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/post/post_update");
+		return mav;
+	}
+	
+	/* 수정한 게시글 업데이트하기 */
+	@PutMapping("/post/edit.do")
+	public ModelAndView updateToDB(BoardDTO board) {
+		ModelAndView mav = new ModelAndView();
+		board.setUpdateDate(new Date());
+		service.savePost(board);
+		mav.setViewName("redirect:/board/list");
+		return mav;
+	}
+	
+	/* 게시글 삭제하기 */
+	@DeleteMapping("/post")
+	public ModelAndView deletePost(@RequestParam("id") int postNumber) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:/board/list");
 		return mav;
 	}
 }
